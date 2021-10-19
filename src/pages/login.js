23a9c9e047f3f1453d27254d76f7b4dbe2cadc98
Alex Velazquez <avelasquez075@gmail.com>
2021-10-19 14:27:35 -0700

    add state values for email/password/isInvalid

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,12 @@ import { useHistory } from "react-router-dom"
 import FirebaseContext from "../context/firebase"
 
 export default function Login() {
+	const [emailAddress, setEmailAddress] = useState("")
+	const [password, setPassword] = useState("")
+
+	const [error, setError] = useState("")
+	const isInvalid = password === "" || emailAddress === ""
+
 	const history = useHistory()
 	const { firebase } = useContext(FirebaseContext)
 
