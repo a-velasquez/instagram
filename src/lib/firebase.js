680429f9e125dde0af09c1762d7f862e0e6249d5
Alex Velazquez <avelasquez075@gmail.com>
2021-10-19 12:31:46 -0700

    add firebase config and import in index.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,8 +1,17 @@
-import Firebase from "firebase/app"
-import "firebase/firestore"
-import "firebase/auth"
+import Firebase from "firebase/compat/app"
+import "firebase/compat/firestore"
+import "firebase/compat/auth"
 
-const config = {}
+const config = {
+	apiKey: "AIzaSyBfi2dq71nYYZRlKDWgpa7lvyXBXOh-qXo",
+	authDomain: "instagram-1138a.firebaseapp.com",
+	projectId: "instagram-1138a",
+	storageBucket: "instagram-1138a.appspot.com",
+	messagingSenderId: "498465647294",
+	appId: "1:498465647294:web:1ceb0b44d19bdeb0bbccef"
+}
 
 const firebase = Firebase.initializeApp(config)
 const { FieldValue } = Firebase.firestore
+
+export { firebase, FieldValue }
