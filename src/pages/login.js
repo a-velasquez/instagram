c5f1146c792fe21b56c7cc18885af26e0f269345
Alex Velazquez <avelasquez075@gmail.com>
2021-10-20 00:01:10 -0700

    add form w/email, password, submit button

--- src/pages/login.js
+++ src/pages/login.js
@@ -23,7 +23,36 @@ export default function Login() {
 			<div className='flex w-3/5'>
 				<img src='/images/iphone-with-profile.jpg' alt='iPhone' />
 			</div>
-			<div className='flex flex-col w-2/5'>Login form goes here</div>
+			<div className='flex flex-col w-2/5'>
+				<h1 className='flex justify-center w-full'>
+					<img src='/images/logo.png' alt='instagram' className='mt2' />
+				</h1>
+				{error && <p className='mb-4 text-xs text-red-primary'>{error}</p>}
+				<form onSubmit={handleLogin} method='POST'>
+					<input
+						aria-label='Enter Your Email Address'
+						type='text'
+						placeholder='Email address'
+						className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+						onChange={({ target }) => setEmailAddress(target.value)}
+					/>
+					<input
+						aria-label='Enter Your Password'
+						type='password'
+						placeholder='Password'
+						className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+						onChange={({ target }) => setPassword(target.value)}
+					/>
+					<button
+						disabled={isInvalid}
+						type='submit'
+						className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${
+							isInvalid && "opacity-50"
+						}`}>
+						Log In
+					</button>
+				</form>
+			</div>
 		</div>
 	)
 }
