a0b9e66ab845e9acf920a3f4e4cb45211a58c336
Alex Velazquez <avelasquez075@gmail.com>
2021-10-20 13:22:40 -0700

    remove old code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import FirebaseContext from "./context/firebase"
 import { firebase, FieldValue } from "./lib/firebase"
-// import "./styles/app.css"
 
 ReactDOM.render(
 	<FirebaseContext.Provider value={{ firebase, FieldValue }}>
