ca46cdd795effe116992b0322cd947960a007d1e
Alex Velazquez <avelasquez075@gmail.com>
2021-10-20 20:45:10 -0700

    add doesUsernameExist(), get collection from firebase

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -2,6 +2,7 @@ import { useState, useContext, useEffect } from "react"
 import { Link, useHistory } from "react-router-dom"
 import FirebaseContext from "../context/firebase"
 import * as ROUTES from "../constants/routes"
+import { doesUsernameExist } from "../servies/firebase"
 
 export default function SignUp() {
 	const history = useHistory()
@@ -11,21 +12,13 @@ export default function SignUp() {
 	const [fullName, setFullName] = useState("")
 	const [emailAddress, setEmailAddress] = useState("")
 	const [password, setPassword] = useState("")
-
 	const [error, setError] = useState("")
 	const isInvalid = password === "" || emailAddress === ""
 
 	const handleSignUp = async (e) => {
 		e.preventDefault()
-		// try {
-		// 	await firebase.auth().signInWithEmailAndPassword(emailAddress, password)
-		// 	history.push(ROUTES.DASHBOARD)
-		// } catch (error) {
-		// 	setEmailAddress("")
-		// 	setPassword("")
-		// 	setError(error.message)
-		// 	console.log(error)
-		// }
+
+		const usernameExists = await doesUsernameExist(username)
 	}
 
 	useEffect(() => {
