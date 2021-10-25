58dbc136dcda52b85fc2e53e9bce0bc779519111
Alex Velazquez <avelasquez075@gmail.com>
2021-10-24 23:42:49 -0700

    add FirebaseContext, UserContext, Header w/styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,35 @@
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+import FirebaseContext from "../context/firebase"
+import UserContext from "../context/user"
+import * as ROUTES from "../constants/routes"
+
 export default function Header() {
-	return <p>Header Component</p>
+	const { firebase } = useContext(FirebaseContext)
+	const { user } = useContext(UserContext)
+
+	console.log("user", user)
+
+	return (
+		<header className='h-16 bg-white border-b border-gray-primary mb-8'>
+			<div className='container mx-auto max-w-screen-lg h-full'>
+				<div className='flex justify-between h-full'>
+					<div className='text-gray-700 text-center flex items-center align-items cursor-pointer'>
+						<h1 className='flex justify-center w-full'>
+							<Link to={ROUTES.DASHBOARD} aria-label='Instagram logo'>
+								<img
+									src='/images/logo.png'
+									alt='Instagram'
+									className='mt-2 w-6/12'
+								/>
+							</Link>
+						</h1>
+					</div>
+					<div>
+						<p>Log In / Log Out</p>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
 }
