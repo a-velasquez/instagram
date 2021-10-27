9f8a95065c7e25ae8b9126ae3670e92147cea3c4
Alex Velazquez <avelasquez075@gmail.com>
2021-10-27 11:12:28 -0700

    move sidebar to index, add User & Suggestions components

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,16 @@
+import useUser from "../../hooks/use-user"
+import User from "../sidebar/user"
+import Suggestions from "../sidebar/suggestions"
+
+export default function Sidebar() {
+	const {
+		user: { fullName, username, userId }
+	} = useUser()
+
+	return (
+		<div className='p-4'>
+			<User />
+			<Suggestions />
+		</div>
+	)
+}
