ee0d92e3f0062904a4a6763a2f883a5b204e1f01
Alex Velazquez <avelasquez075@gmail.com>
2021-10-27 15:12:55 -0700

    add useState, useEffect, prop-types and prof state value

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -1,3 +1,12 @@
+import { useState, useEffect } from "react"
+import PropTypes from "prop-types"
+
 export default function Suggestions() {
+	const [profile, setProfile] = useState(null)
+
 	return <p>Suggestions Component</p>
+}
+
+Suggestions.propTypes = {
+	userId: PropTypes.string
 }
