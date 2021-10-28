2d886c6d8f066bee960b217092a9266c5891c8ad
Alex Velazquez <avelasquez075@gmail.com>
2021-10-28 13:37:19 -0700

    grab docId from user, pass to Suggestions as loggedInUserDocId

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,13 +4,17 @@ import useUser from "../../hooks/use-user"
 
 export default function Sidebar() {
 	const {
-		user: { fullName, username, userId, following }
+		user: { fullName, username, userId, following, docId }
 	} = useUser()
 
 	return (
 		<div className='p-4'>
 			<User username={username} fullName={fullName} />
-			<Suggestions userId={userId} following={following} />
+			<Suggestions
+				userId={userId}
+				following={following}
+				loggedInUserDocId={docId}
+			/>
 		</div>
 	)
 }
