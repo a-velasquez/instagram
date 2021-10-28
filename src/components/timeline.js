f8b57531092c985226a08fe33c75263e31377918
Alex Velazquez <avelasquez075@gmail.com>
2021-10-28 14:44:46 -0700

    add notes, and imports for skeleton, usePhotos hook

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,17 @@
+import Skeleton from "react-loading-skeleton"
+import usePhotos from "../hooks/use-photos"
+
 export default function Timeline() {
+	// TODO
+	// need to grab logged in user's photos
+	// use react skeleton for loading photos
+	// if photos, render post component
+	// if !photos, ask user to upload photo to post
+
 	return (
 		<div className='container col-span-2'>
 			<p>Timeline Component</p>
+			{/* {photos} */}
 		</div>
 	)
 }
