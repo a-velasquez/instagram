be9a1508af243ad0c13271355be9afeab4e8d58c
Alex Velazquez <avelasquez075@gmail.com>
2021-10-27 23:33:11 -0700

    deconstruct following and pass to Suggestions

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,16 +1,16 @@
-import useUser from "../../hooks/use-user"
 import User from "../sidebar/user"
 import Suggestions from "../sidebar/suggestions"
+import useUser from "../../hooks/use-user"
 
 export default function Sidebar() {
 	const {
-		user: { fullName, username, userId }
+		user: { fullName, username, userId, following }
 	} = useUser()
 
 	return (
 		<div className='p-4'>
 			<User username={username} fullName={fullName} />
-			<Suggestions userId={userId} />
+			<Suggestions userId={userId} following={following} />
 		</div>
 	)
 }
