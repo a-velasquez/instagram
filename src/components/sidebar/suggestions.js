caef35ae7dfbfc3b617f1507ee3864af559dc98e
Alex Velazquez <avelasquez075@gmail.com>
2021-10-27 20:10:35 -0700

    add nested ternary for profile suggestions, profiles state

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -1,10 +1,20 @@
 import { useState, useEffect } from "react"
 import PropTypes from "prop-types"
+import Skeleton from "react-loading-skeleton"
 
-export default function Suggestions() {
-	const [profile, setProfile] = useState(null)
+export default function Suggestions({ userId }) {
+	const [profiles, setProfiles] = useState(null)
 
-	return <p>Suggestions Component</p>
+	// return profiles
+	return !profiles ? (
+		<Skeleton count={10} height={150} className='mt-5' />
+	) : profiles.length > 0 ? (
+		<div className='rounded flex flex-col'>
+			<div className='text-sm flex items-center align-items justify-between mb-2'>
+				<p className='font-bold text-gray-base'>Suggestions for you</p>
+			</div>
+		</div>
+	) : null
 }
 
 Suggestions.propTypes = {
