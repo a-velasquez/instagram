5032a14a5f22f586276763aa44a96047917c8811
Alex Velazquez <avelasquez075@gmail.com>
2021-10-28 21:31:06 -0700

    import actions, pass docId, totalLikes, likedPhoto, handleFocus props

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,14 +1,24 @@
+import { useRef } from "react"
 import PropTypes from "prop-types"
 import Header from "./header"
 import Image from "./image"
+import Actions from "./actions"
 
 export default function Post({ content }) {
-	// components needed:
-	// need post header, actions (like and comment), image, footer and comments
+	const commentInput = useRef(null)
+
+	const handleFocus = () => commentInput.current.focus()
+
 	return (
 		<div className='rounded col-span-4 border bg-white border-gray-primary mb-12'>
 			<Header username={content.username} />
 			<Image src={content.imageSrc} caption={content.caption} />
+			<Actions
+				docId={content.docId}
+				totalLikes={content.likes.length}
+				likedPhoto={content.userLikedPhoto}
+				handleFocus={handleFocus}
+			/>
 		</div>
 	)
 }
