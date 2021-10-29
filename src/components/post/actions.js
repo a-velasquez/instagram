c2666821043567d895833a00781a9b708a37adad
Alex Velazquez <avelasquez075@gmail.com>
2021-10-28 20:58:31 -0700

    add state, context, firebase, props needed

---
+++ src/components/post/actions.js
@@ -0,0 +1,13 @@
+import { useState, useContext } from "react"
+import PropTypes from "prop-types"
+import FirebaseContext from "../../context/firebase"
+import UserContext from "../../context/user"
+
+export default function Actions({
+	docId,
+	totalLikes,
+	likedPhoto,
+	handleFocus
+}) {
+	return null
+}
