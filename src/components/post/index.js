8897f655dd55dc5b6151751c6dc9e2f4551f6703
Alex Velazquez <avelasquez075@gmail.com>
2021-10-28 19:40:16 -0700

    add PropTypes needed for each post

---
+++ src/components/post/index.js
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types"
+
+export default function Post() {
+	return <p>Post</p>
+}
+
+Post.propTypes = {
+	content: PropTypes.shape({
+		username: PropTypes.string.isRequired,
+		imageSrc: PropTypes.string.isRequired,
+		caption: PropTypes.string.isRequired,
+		docId: PropTypes.string.isRequired,
+		userLikedPhoto: PropTypes.bool.isRequired,
+		likes: PropTypes.array.isRequired,
+		comments: PropTypes.array.isRequired,
+		dateCreated: PropTypes.number.isRequired
+	})
+}
