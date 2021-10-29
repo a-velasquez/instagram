fc69c1cd3bddf2ff438d83d2c0c99fae96e599d2
Alex Velazquez <avelasquez075@gmail.com>
2021-10-28 20:20:04 -0700

    add notes and Header component, pass content.username

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types"
+import Header from "./header"
 
 export default function Post({ content }) {
+	// components needed:
+	// need post header, actions (like and comment), image, footer and comments
 	return (
-		<div>
-			<p>Post</p>
+		<div className='rounded col-span-4 border bg-white border-gray-primary mb-12'>
+			<Header username={content.username} />
 			<img src={content.imageSrc} />
 		</div>
 	)
