5fe62cc0820dd19ec9f0b76131fb66e1cfb0756b
Alex Velazquez <avelasquez075@gmail.com>
2021-10-28 20:26:23 -0700

    move image to Image component

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import Header from "./header"
+import Image from "./image"
 
 export default function Post({ content }) {
 	// components needed:
@@ -7,7 +8,7 @@ export default function Post({ content }) {
 	return (
 		<div className='rounded col-span-4 border bg-white border-gray-primary mb-12'>
 			<Header username={content.username} />
-			<img src={content.imageSrc} />
+			<Image src={content.imageSrc} />
 		</div>
 	)
 }
