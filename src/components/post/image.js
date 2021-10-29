c194a6ef7329e2c5515fb16b455fc4e9dd4773f1
Alex Velazquez <avelasquez075@gmail.com>
2021-10-28 20:52:46 -0700

    add prop-types and pass caption to Image

--- src/components/post/image.js
+++ src/components/post/image.js
@@ -1,3 +1,10 @@
-export default function Image({ src }) {
-	return <img src={src} alt='' />
+import PropTypes from "prop-types"
+
+export default function Image({ src, caption }) {
+	return <img src={src} alt={caption} />
+}
+
+Image.propTypes = {
+	src: PropTypes.string.isRequired,
+	caption: PropTypes.string.isRequired
 }
