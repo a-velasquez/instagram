7017ff2abc812662eb95dc35ba0c3936f1adebf5
Alex Velazquez <avelasquez075@gmail.com>
2021-10-28 19:35:31 -0700

    destructure following, sort photos by date, call getTimelinePhotos()

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -10,15 +10,19 @@ export default function usePhotos() {
 
 	useEffect(() => {
 		async function getTimelinePhotos() {
-			const { following } = await getUserByUserId(userId)
+			const [{ following }] = await getUserByUserId(userId)
 			let followedUserPhotos = []
 
+			console.log("following", following)
 			if (following.length > 0) {
 				followedUserPhotos = await getPhotos(userId, following)
 			}
+			followedUserPhotos.sort((a, b) => b.dateCreate - a.dateCreated)
+			setPhotos(followedUserPhotos)
 		}
+		getTimelinePhotos()
 		console.log(userId)
-	}, [])
+	}, [userId])
 
 	return { photos }
 }
