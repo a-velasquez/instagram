00a9635868cbcbd39c13a7b195cde096785347e9
Alex Velazquez <avelasquez075@gmail.com>
2021-10-30 13:04:12 -0700

    alias comments, add state, map comments and links to profiles

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -1,9 +1,32 @@
 import { useState } from "react"
 import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
+import { formatDistance } from "date-fns"
 
-export default function Comments({ docId, commentInput, comments, posted }) {
-	return <p>Comments</p>
+// aliased comments prop (allComments) there will be
+// a state value of comments, setComments
+export default function Comments({
+	docId,
+	comments: allComments,
+	posted,
+	commentInput
+}) {
+	const [comments, setComments] = useState(allComments)
+
+	return (
+		<>
+			<div className='p-4 pt-1 pb-4'>
+				{comments.map((item) => (
+					<p key={`${item.comment}-${item.displayName}`} className='mb-1'>
+						<Link to={`/p/${item.displayName}`}>
+							<span className='mr-1 font-bold'>{item.displayName}</span>
+						</Link>
+						<span>{item.comment}</span>
+					</p>
+				))}
+			</div>
+		</>
+	)
 }
 
 Comments.propTypes = {
