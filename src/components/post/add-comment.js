de18f521c3248ca5b7384923698de8559e0eb29e
Alex Velazquez <avelasquez075@gmail.com>
2021-10-30 14:01:05 -0700

    add firebase state, userContext, propTypes

--- src/components/post/add-comment.js
+++ src/components/post/add-comment.js
@@ -10,6 +10,24 @@ export default function AddComment({
 	commentInput
 }) {
 	const [comment, setComment] = useState("")
+	const { firebase, FieldValue } = useContext(FirebaseContext)
+	// just checking the auth, dont need info from firestore
+	const {
+		user: { displayName }
+	} = useContext(UserContext)
+
+	const handleSubmitComment = (e) => {
+		e.preventDefault()
+
+		return null
+	}
 
 	return null
+}
+
+AddComment.propTypes = {
+	docId: PropTypes.string.isRequired,
+	comments: PropTypes.array.isRequired,
+	setComments: PropTypes.func.isRequired,
+	commentInput: PropTypes.object
 }
