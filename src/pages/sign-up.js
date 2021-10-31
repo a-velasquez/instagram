5b9231975ff7a2d2d07a255feb1f8a96c7a1e662
Alex Velazquez <avelasquez075@gmail.com>
2021-10-30 23:58:04 -0700

    update import file path

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -2,7 +2,7 @@ import { useState, useContext, useEffect } from "react"
 import { Link, useHistory } from "react-router-dom"
 import FirebaseContext from "../context/firebase"
 import * as ROUTES from "../constants/routes"
-import { doesUsernameExist } from "../servies/firebase"
+import { doesUsernameExist } from "../services/firebase"
 
 export default function SignUp() {
 	const history = useHistory()
