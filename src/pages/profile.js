77d6963e726a0ba19c7c815da09ae189b24a446b
Alex Velazquez <avelasquez075@gmail.com>
2021-10-31 00:18:01 -0700

    add baseline for Profile page, state, css, render usename of profile

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,31 @@
-import { useContext, useEffect, useState } from "react"
+import { useState, useEffect } from "react"
+import { useParams, useHistory } from "react-router-dom"
+import { getUserByUsername } from "../services/firebase"
+import * as ROUTES from "../constants/routes"
 
 export default function Profile() {
-	return null
+	const { username } = useParams()
+	const history = useHistory()
+	const [user, setUser] = useState(null)
+
+	useEffect(() => {
+		async function checkUserExists() {
+			const [user] = await getUserByUsername(username)
+			if (user?.userId) {
+				setUser(user)
+			} else {
+				history.push(ROUTES.NOT_FOUND)
+			}
+		}
+
+		checkUserExists()
+	}, [username, history])
+
+	return user?.username ? (
+		<div className='bg-gray-background'>
+			<div className='mx-auto max-w-screen-lg'>
+				<p>{username}</p>
+			</div>
+		</div>
+	) : null
 }
