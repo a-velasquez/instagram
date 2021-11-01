87eef92f02335ab35949bf8afa1b2a8734647d0a
Alex Velazquez <avelasquez075@gmail.com>
2021-10-31 20:03:46 -0700

    add skeleton, map over photos, need to add styling

---
+++ src/components/profile/photos.js
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types"
+import Skeleton from "react-loading-skeleton"
+
+export default function Photos({ photos }) {
+	return (
+		<div>
+			{photos.map((photo) => (
+				<div key={photo.docId} className='relative group'>
+					<img src={photo.imageSrc} alt={photo.caption} />
+				</div>
+			))}
+		</div>
+	)
+}
