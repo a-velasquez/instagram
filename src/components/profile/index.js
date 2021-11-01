9387fed78b4e7ba88ba53c6c5f452b8f1f055c87
Alex Velazquez <avelasquez075@gmail.com>
2021-10-31 19:23:42 -0700

    add reducer, initialState, useEffect (unfinished)

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -2,6 +2,21 @@ import { useReducer, useEffect } from "react"
 import PropTypes from "prop-types"
 import Header from "./header"
 
-export default function Profile() {
-	return null
+export default function Profile({ username }) {
+	const reducer = (state, newState) => ({ ...state, ...newState })
+	const intialState = {
+		profile: {},
+		photosCollection: [],
+		followerCount: 0
+	}
+	const [{ profile, photosCollection, followerCount }, dispatch] = useReducer(
+		reducer,
+		initialState
+	)
+
+	useEffect(() => {
+		async function getProfileInfoAndPhotos() {
+			return null
+		}
+	}, [])
 }
