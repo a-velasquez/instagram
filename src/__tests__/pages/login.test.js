c9cb9f64c1e6182d644e70ee9019fd2ca1ff29de
Alex Velazquez <avelasquez075@gmail.com>
2021-11-06 00:12:43 -0700

    add baseline test to see if testing is working

---
+++ src/__tests__/pages/login.test.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { render, fireEvent, waitFor } from "@testing-library/react"
+
+describe("<Login />", () => {
+	it("renders the login page with a form submission and logs the user in", () => {
+		expect(1).toEqual(1)
+	})
+})
